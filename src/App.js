d8f8d41d3f68783a11d9539a8e3f5ea194310a70
christopherkade <c.kade96@gmail.com>
2019-03-08 10:51:33 +0100

    Shuffle array

--- src/App.js
+++ src/App.js
@@ -15,18 +15,31 @@ class App extends Component {
   }
 
   /**
+   * Shuffles our restaurant array and saves it
+   * @param {*} array 
+   */
+  shuffleArray(array) {
+    let newArray = []
+    while (array.length !== 0) {
+      let randomIndex = Math.floor(Math.random() * array.length)
+      newArray.push(array[randomIndex])
+      array.splice(randomIndex, 1)    
+    }
+
+    this.setState({
+      restaurants: newArray
+    })
+  }
+
+  /**
    * On component mount, fetch and save the Prismic restaurant data
    */
   componentDidMount() {
-    console.log("Component has mounted, fetching restaurant data");
-
     Prismic.getApi("https://lunch-roulette.prismic.io/api/v2").then((api) => {
       return api.query(Prismic.Predicates.at('document.type', 'restaurant'));
     }).then((response) => {
       const restaurantData = response.results.map((res) => res.data)
-      this.setState({
-        restaurants: restaurantData
-      })
+      this.shuffleArray(restaurantData)
     })
   }
 
