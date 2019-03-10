914fd25c1ace5be5f67f01d5ea559ac436bddf2c
christopherkade <c.kade96@gmail.com>
2019-03-10 13:21:08 +0100

    Fixed linting errors

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,13 @@ import React from 'react'
 import './Footer.css'
 
 function Footer() {
-    return (
-        <div className="footer-wrapper">
-            <h3>
-                Made with ♥ by <a className="footer-link" href="https://twitter.com/christo_kade">Christopher Kade</a>
-            </h3>
-        </div>
-    )
+  return (
+    <div className="footer-wrapper">
+      <h3>
+        Made with ♥ by <a className="footer-link" href="https://twitter.com/christo_kade">Christopher Kade</a>
+      </h3>
+    </div>
+  )
 }
 
 export default Footer
