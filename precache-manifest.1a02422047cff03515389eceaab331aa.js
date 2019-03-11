ab62dae9fe4b6352561f2f248f5a75f78da73906
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-11 17:38:56 +0000

    Deploy christopherkade/zenika-lunch-roulette to github.com/christopherkade/zenika-lunch-roulette.git:gh-pages

---
+++ precache-manifest.1a02422047cff03515389eceaab331aa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bab732c217955f87f5a9ae437ce9469",
+    "url": "/zenika-lunch-roulette/static/media/fork.8bab732c.svg"
+  },
+  {
+    "revision": "f1467161ffc84f96cdd9",
+    "url": "/zenika-lunch-roulette/static/css/main.f278665c.chunk.css"
+  },
+  {
+    "revision": "3d53ad576fda20755c8c",
+    "url": "/zenika-lunch-roulette/static/js/runtime~main.e3533ff2.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/zenika-lunch-roulette/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9f4793623dff946aff69",
+    "url": "/zenika-lunch-roulette/static/js/2.d90b55e6.chunk.js"
+  },
+  {
+    "revision": "d69e26f277d200edf4acf6adedfbe4ac",
+    "url": "/zenika-lunch-roulette/static/media/logo.d69e26f2.png"
+  },
+  {
+    "revision": "f1467161ffc84f96cdd9",
+    "url": "/zenika-lunch-roulette/static/js/main.a05418bf.chunk.js"
+  },
+  {
+    "revision": "9367e4e38fa691cc35e043edc571e282",
+    "url": "/zenika-lunch-roulette/static/media/google.9367e4e3.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/zenika-lunch-roulette/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/zenika-lunch-roulette/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/zenika-lunch-roulette/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "9f4793623dff946aff69",
+    "url": "/zenika-lunch-roulette/static/css/2.bedd7820.chunk.css"
+  },
+  {
+    "revision": "0e53f1b3a84d2508d166c2c9fda1732d",
+    "url": "/zenika-lunch-roulette/index.html"
+  }
+];
